9bd69babf5ed32180cbcc8d25fa794baa5959686
Kevin Serrano <kevinserrano010@gmail.com>
2024-11-17 23:11:05 -0300

    Creacion de proyecto Fulbo App

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import  Icon  from 'react-native-vector-icons/MaterialIcons'
+import { colors } from '../global/colors';
+
+const Header = () => {
+
+    const onLogout = () => {
+        console.log("Cerrando sesión");
+    };
+
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>Ecommerce</Text>
+      <Pressable onPress={onLogout} style={styles.access}><Icon name="logout" size={20} color="#fff" /></Pressable>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        width: '100%',
+        height: 110,
+        paddingTop: 50,
+        backgroundColor: colors.Naranja,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // borderBottomLeftRadius: 25,
+        // borderBottomRightRadius: 25,
+    },
+    title: {
+        color: colors.Negro,
+        fontWeight: 'bold',
+        fontSize: 21
+    },
+    access: {
+      alignSelf: 'flex-end',
+      paddingRight: 16
+    }
+})
---
+++ src/global/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  AzulOscuro: "#180161",
+  AzulClaro: "#3a4f76",
+  Blanco: "#ffffff",
+  Gris: "#c7c7c7",
+  Rojo: "#f7287b",
+  Verde: "#4caf50",
+  Negro: "#000000",
+  Amarillo: "#FFF100",
+  Naranja: "#FF7D29",
+};
