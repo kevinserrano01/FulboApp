3b04dc053a4fa55d9048315fdb2306a91f810f65
Kevin Serrano <kevinserrano010@gmail.com>
2024-11-18 15:18:59 -0300

    Creacion de RegisterScreen

--- src/screens/auth/RegisterScreen.jsx
+++ src/screens/auth/RegisterScreen.jsx
@@ -1,13 +1,141 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, ImageBackground, Pressable } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from '../../global/colors';
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>RegisterScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <ImageBackground
+          source={require('../../../assets/welcome.jpg')}
+          style={styles.imageBackground}
+        >
+          <LinearGradient
+            colors={['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.5)']}
+            style={styles.gradient}
+          >
+            <Text style={styles.title}>Fulbo</Text>
+          </LinearGradient>
+        </ImageBackground>
+      </View>
+      <View style={styles.formContainer}>
+        <Text style={styles.subtitle}>Registrarse</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre de usuario"
+          placeholderTextColor="#ccc"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#ccc"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          placeholderTextColor="#ccc"
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Registrarme</Text>
+        </Pressable>
+        <Pressable style={styles.registerButton}>
+          <Text style={styles.registerButtonText}>Ingresar como invitado</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'start',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    width: '100%',
+    height: '40%',
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // borderBottomLeftRadius: 45,
+    // borderBottomRightRadius: 41,
+    // overflow: 'hidden',
+  },
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  formContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 60,
+  },
+  subtitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 50,
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+    color: '#333',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    width: '100%',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: colors.Verde,
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  registerButton: {
+    backgroundColor: 'transparent',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: '80%',
+    alignItems: 'center',
+  },
+  registerButtonText: {
+    color: colors.Negro,
+    fontSize: 15,
+    textDecorationLine: 'underline',
+  },
+})
