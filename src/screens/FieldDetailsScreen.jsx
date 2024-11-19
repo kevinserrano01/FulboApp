8f6ed7112677aa0d140daa238890bffa58e1646f
Kevin Serrano <kevinserrano010@gmail.com>
2024-11-19 18:16:09 -0300

    Actualizacion de componente en canchas

--- src/screens/FieldDetailsScreen.jsx
+++ src/screens/FieldDetailsScreen.jsx
@@ -1,13 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
 
-const FieldDetailsScreen = () => {
+const FieldDetailsScreen = ({route}) => {
+  const { nombre, ubicacion, tipo, coordenadas, descripcion, precio, opiniones } = route.params;
+
   return (
-    <View>
-      <Text>FieldScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {coordenadas && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: coordenadas.latitude,
+            longitude: coordenadas.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{ latitude: coordenadas.latitude, longitude: coordenadas.longitude }}
+            title={nombre}
+            description={tipo}
+          />
+        </MapView>
+      )}
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}> 📌 {nombre}</Text>
+        <Text style={styles.info}>📍 {ubicacion}</Text>
+        <Text style={styles.info}> ⚽️ {tipo}</Text>
+        <Text style={styles.info}> ⚽️ {descripcion}</Text>
+        <Text style={styles.info}> ⚽️ {precio}</Text>
+        <Text style={styles.info}> Opiniones </Text>
+        <FlatList 
+          data={opiniones}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <View>
+              <Text>{item.usuario}</Text>
+              <Text>{item.comentario}</Text>
+              <Text>{item.puntuacion}</Text>
+            </View>
+          )}
+        />
+
+      </View>
+      
+      
+    </ScrollView>
   )
 }
 
 export default FieldDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  map: {
+    width: '100%',
+    height: 600,
+  },
+  info: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+})
--- src/screens/FieldsScreen.jsx
+++ src/screens/FieldsScreen.jsx
@@ -5,62 +5,22 @@ import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 
-const FieldsScreen = ({navigation}) => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+const FieldsScreen = ({navigation}) => {    
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Se denegó el permiso para acceder a la ubicación');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  const renderFieldItem = ({item}) => {
-    const { coordenadas } = item;
-    if (!coordenadas) {
+    const renderFieldItem = ({item}) => {    
       return (
-        <View style={styles.flatContainer}>
-          <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.nombre}</Text>
-          <Text>Coordenadas no disponibles</Text>
-        </View>
-      );
-    }
-    const { latitude, longitude } = coordenadas; // Asumiendo que ubicacion tiene latitud y longitud
-    
-    return (
-      <Pressable onPress={() => navigation.navigate('Cancha', item.nombre)}>
-        <FlatCard style={styles.flatContainer}>
-          <MapView
-            style={{ width: '100%', height: 200 }}
-            initialRegion={{
-              latitude: latitude,
-              longitude: longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          >
-            <Marker
-              coordinate={{ latitude: latitude, longitude: longitude }}
-              title={item.nombre}
-              description={item.tipo}
-            />
-          </MapView>
-          <View style={{padding: 10}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.nombre}</Text>
-            <Text>{item.ubicacion}</Text>
-            <Text>{item.tipo}</Text>
-          </View>
-        </FlatCard>
-      </Pressable>
-    )
-  };
+        <Pressable onPress={() => navigation.navigate('Cancha', {...item})}>
+          <FlatCard style={styles.flatContainer}>
+          <Image source={require('../../assets/icono-futbol.png')} style={{width: 190, height: 180}} />
+            <View style={{padding: 10}}>
+              <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.nombre}</Text>
+              <Text>{item.ubicacion}</Text>
+              <Text>{item.tipo}</Text>
+            </View>
+          </FlatCard>
+        </Pressable>
+      )
+    };
   return (
     <>
       { canchas.length > 0 ?
@@ -81,8 +41,8 @@ export default FieldsScreen
 const styles = StyleSheet.create({
   flatContainer: {
     margin: 10,
-    padding: 10,
-    flexDirection: 'column',
-    height: 300,
+    padding: 5,
+    flexDirection: 'row',
+    height: 200,
 },
 })
