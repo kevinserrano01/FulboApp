4445fcd929267664a080101258da1da738d55a85
Kevin Serrano <kevinserrano010@gmail.com>
2024-11-26 19:58:06 -0300

    Creacion de pantalla de carga con 3 segundos + Creacion de icono

--- App.jsx
+++ App.jsx
@@ -4,8 +4,28 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import MainNavigator from "./src/navigation/MainNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/app/store";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      await SplashScreen.hideAsync();
+      setIsReady(true);
+    }, 3000); // esperar 3 segundos antes de mostrar la pantalla principal
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isReady) {
+    return null; // renderizar una pantalla de carga
+  }
+
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
